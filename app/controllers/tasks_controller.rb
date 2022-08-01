c9a4cc4c4af8b49922a0154b80da8dc42f8ba719
ayumi nakano <aynminoa@gmail.com>
2022-08-01 20:56:52 +0900

    タスクCRUD実装

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    if params[:back]
+      @task = Task.new(task_params)
+    else
+      @task = Task.new
+    end
+  end
+
+  def edit
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "登録が完了しました"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを更新しました" 
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+      redirect_to tasks_path, notice: "タスクを削除しました"
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
+end
